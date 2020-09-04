cd7d025302adedff131c8011604688bbfd9977f8
michaelmdrs <mackelldeveloper@gmail.com>
2020-09-04 13:00:23 -0300

    Exemplos de como usar o This

--- CursoJavaScript/funcao/thisEBind2.js
+++ CursoJavaScript/funcao/thisEBind2.js
@@ -1,8 +1,25 @@
 function Pessoa() {
     this.idade = 0
 
+    const self = this
     setInterval(function () {
-        this.idade++
-        console.log(this.idade)
-    }.bind(this), 1000)
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
 }
+
+new Pessoa
+
+/*
+function Cliente() {
+    this.idade = 0
+
+    setInterval(function() {
+        this.idade++ // Incrementando o valor da idade
+        console.log(this.idade)
+    }, 1000)
+}
+
+new Cliente
+
+*/
