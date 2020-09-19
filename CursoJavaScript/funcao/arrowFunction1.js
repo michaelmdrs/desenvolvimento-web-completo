46aed61a3b6839a7294a36dfbd4304f21a72c2cb
michaelmdrs <mackelldeveloper@gmail.com>
2020-09-19 00:21:29 -0300

    funcao arrow

---
+++ CursoJavaScript/funcao/arrowFunction1.js
@@ -0,0 +1,16 @@
+let dobro = function(a) {
+	return 2 * a
+}
+
+dobro = (a) => {  
+	return 2 * a
+}
+
+dobro = a => 2 * a // Função em uma única linha
+console.log(dobro(Math.PI))
+
+let ola = function () {
+	return 'Olá'
+}
+
+ola = () => 'Olá, Mundo!'
