3a043d269182bfed5c54156697475141671496bc
Michael <mackelldeveloper@gmail.com>
2020-10-01 23:55:29 -0300

    Introdução ao map

---
+++ CursoJavaScript/array/map1.js
@@ -0,0 +1,12 @@
+const nums = [1, 2, 3, 4, 5]
+
+// O Map é um For com propósito
+let resultado = nums.map(function(e) {
+    return e * 2
+})
+
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.' , ',')}`
