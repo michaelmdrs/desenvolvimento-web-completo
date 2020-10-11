b57f59eefdd4d4a4bbdd6840a6d0e0b201790859
Michael <mackelldeveloper@gmail.com>
2020-10-11 10:17:56 -0300

    Imperativo vs Declarativo
    Exemplo de como utilizar códigos imperativos e declarativos.

--- CursoJavaScript/array/imperativoVsDeclarativo.js
+++ CursoJavaScript/array/imperativoVsDeclarativo.js
@@ -13,4 +13,9 @@ for (let i = 0 ; i < alunos.length; i++) {
 console.log(total1 / alunos.length)
 
 // Declarativo
-const getNota = aluno => 
+
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+
+const total2 = alunos.map(getNota).reduce(soma)
+console.log(total2 / alunos.length)
