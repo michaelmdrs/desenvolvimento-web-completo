783a60b0a8dea2f2c3c758f07b3897cf14299117
Michael <mackelldeveloper@gmail.com>
2020-11-01 23:59:31 -0300

    Adicao de comentarios para estudos

--- CursoJavaScript/array/array.js
+++ CursoJavaScript/array/array.js
@@ -1,12 +1,15 @@
-let aprovados = new Array('Nara', 'Néfi', 'Anna')
+let aprovados = new Array('Nara', 'Néfi', 'Anna') // Forma não recomendada
 console.log(aprovados)
 
-aprovados = ['Nara', 'Néfi', 'Anna']
-console.log(aprovados)
+aprovados = ['Nara', 'Néfi', 'Anna'] // Notação literal (Recomendada)
+//console.log(aprovados)
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3]) // Não existe porém o JS retorna valor undefined
 
-
-aprovados[3] = 'Michael'
-aprovados.push('Rebeca')
+aprovados[3] = 'Michael' // Adiciona um novo elemento
+aprovados.push('Rebeca') // A forma mais usual para adicionar um valor ao array
 console.log(aprovados.length)
 
 aprovados[4] = 'Rebeca'
@@ -14,7 +17,7 @@ console.log(aprovados.length)
 console.log(aprovados[5] === undefined)
 
 console.log(aprovados)
-aprovados.sort()
+aprovados.sort() // Ordena o array pela ordem alfabética ou númerica
 console.log(aprovados)
 
 delete aprovados[1]
@@ -22,5 +25,6 @@ console.log(aprovados[1])
 console.log(aprovados[2])
 
 aprovados = ['Nara', 'Néfi', 'Patricia']
-aprovados.splice(1, 1), 
+aprovados.splice(1, 1)
+// aprovados.splice(1, 1, 'Néfi')
 console.log(aprovados)
