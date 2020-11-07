eb1c9471b84eb053fb651085db97aa2ce36ae271
Michael Santos <mackelldev@gmail.com>
2020-11-07 03:07:18 -0300

    Refatoração

--- CursoJavaScript/controle/switch.js
+++ CursoJavaScript/controle/switch.js
@@ -2,32 +2,6 @@ const imprimirResultado = function (nota) {
     switch (Math.floor(nota)) {
         case 10:
         case 9:
-<<<<<<< HEAD
-            console.log('Quadro de Honra')
-            break
-        case 8: case 7:
-            console.log('Aprovado')
-            break
-        case 6: case 5: case 4:
-            console.log('Recuperação')
-            break
-        case 3: case 2: case 1:
-            console.log('Reprovado')
-            break
-        default:
-            console.log('Nota inválida')
-    }
-}
-
-imprimirResultado(10)
-imprimirResultado(8,9)
-imprimirResultado(6.55)
-imprimirResultado(-1)
-imprimirResultado(11)
-imprimirResultado('PASSOU')
-imprimirResultado(7)
-imprimirResultado(3)
-=======
             console.log('QUADRO DE HONRA')
             break
         case 8: case 7:
@@ -49,4 +23,3 @@ imprimirResultado(8.5)
 imprimirResultado(10)
 imprimirResultado(-1)
 imprimirResultado(3)
->>>>>>> d079a374730f4ea36ac821afcca010843914f20b
--- CursoJavaScript/controle/while.js
+++ CursoJavaScript/controle/while.js
@@ -7,11 +7,7 @@ let opcao = 0
 
 while (opcao != -1) {
     opcao = getInteiroAleatorioEntre(-1, 10)
-<<<<<<< HEAD
     console.log(`Opção escolhida foi ${opcao}.`)
-=======
-    console.log(`Opção escolhida foi ${opcao}`)
->>>>>>> d079a374730f4ea36ac821afcca010843914f20b
 }
 
 console.log('Até a próxima')
