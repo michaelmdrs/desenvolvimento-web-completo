ffa31fc2e53f2e03fc0a79b179f0087dc6370a99
Michael <mackelldeveloper@gmail.com>
2021-01-03 21:54:43 -0300

    adicionando exercicios

---
+++ CursoJavaScript/ESNext/revisao1.js
@@ -0,0 +1,18 @@
+// let e const 
+{
+    var a = 1
+    let b = 2
+    console.log(b)
+}
+console.log(a)
+
+// Template String
+const produto = 'Notebook'
+console.log(`${produto} está caro!`)
+
+// Desctructuring 
+const [l, e, ...tras] = 'House'
+console.log(l, e, tras)
+
+const [x, y] = [4, 5, 11]
+console.log(x,y)
